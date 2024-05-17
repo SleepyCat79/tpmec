9b3ca5ff2c4ebaf62ce18e3d8468bf6fb2e878c1
Sleepy <60354077+SleepyCat79@users.noreply.github.com>
2024-05-17 15:43:58 +0700

    fix db

--- src/config/db.js
+++ src/config/db.js
@@ -3,30 +3,29 @@ const mysql2 = require("mysql2");
 let db;
 
 function connectToDatabase() {
-  if (db) {
+  if (!db) {
+    db = mysql2.createConnection({
+      host: "tpmec.czsyoaugy6di.ap-southeast-2.rds.amazonaws.com",
+      port: "3306",
+      user: "admin",
+      password: "tpmec123",
+      database: "TPM_EC",
+    });
+
+    console.log("Connecting for testing BE functionality");
+
+    db.connect((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Connected to database successfully!");
+      }
+    });
+    return db;
+  } else {
     console.log("Already connected to database.");
     return db;
   }
-
-  db = mysql2.createConnection({
-    host: "tpmec.czsyoaugy6di.ap-southeast-2.rds.amazonaws.com",
-    port: "3306",
-    user: "admin",
-    password: "tpmec123",
-    database: "TPM_EC",
-  });
-
-  console.log("Connecting for testing BE functionality");
-
-  db.connect((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Connected to database successfully!");
-    }
-  });
-
-  return db;
 }
 
 module.exports = connectToDatabase;
